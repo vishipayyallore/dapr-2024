aa31bf69473f5fd7c07f6b53d4ba2848fdabfdb7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-13 10:23:17 +0530

    Swamy/11nov work day1 (#2)
    * Initial Version
    
    * Package updates
    
    * Changes
    
    * Changes
    
    * Update README.md
    
    * Update HelloWorldController.cs
    
    * Update HelloWorldController.cs
    
    * Update HelloWorldController.cs
    
    * Changes

--- dapr.microservice.webapi/Controllers/HelloWorldController.cs
+++ dapr.microservice.webapi/Controllers/HelloWorldController.cs
@@ -2,15 +2,16 @@
 
 namespace dapr.microservice.webapi.Controllers;
 
-//[Route("api/[controller]")]
+[Route("api/[controller]")]
 [ApiController]
-public class HelloController(ILogger<HelloController> logger) : ControllerBase
+public class HelloWorldController(ILogger<HelloWorldController> logger) : ControllerBase
 {
-    private readonly ILogger<HelloController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    private readonly ILogger<HelloWorldController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
     [HttpGet("gethello")]
     public ActionResult<string> Get()
     {
+        _logger.LogInformation("Hitting Get Hello Method.");
         Console.WriteLine("Hello, World.");
 
         return "Hello, World";
