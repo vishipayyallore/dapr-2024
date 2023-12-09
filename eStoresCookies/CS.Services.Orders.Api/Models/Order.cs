d3d34c761aa15567b092d45d1609e3866ac794ba
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-09 18:08:48 +0530

    Added Models

---
+++ eStoresCookies/CS.Services.Orders.Api/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CS.Services.Orders.Api.Models;
+
+public class Order
+{
+    public DateTime Date { get; set; }
+
+    public Guid Id { get; set; }
+
+    public string? CustomerCode { get; set; }
+
+    [Required]
+    public List<OrderItem>? Items { get; set; }
+}
---
+++ eStoresCookies/CS.Services.Orders.Api/Models/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CS.Services.Orders.Api.Models;
+
+public class OrderItem
+{
+    [Required]
+    public string? ProductCode { get; set; }
+
+    public int Quantity { get; set; }
+}
