73fa42f945eab58004ff32cb7b8d49ed232af038
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-09 18:05:43 +0530

    Refactoring

--- eStoresCookies/CS.Services.Orders.Api/Program.cs
+++ eStoresCookies/CS.Services.Orders.Api/Program.cs
@@ -25,6 +25,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapSubscribeHandler();
 
 app.Run();
