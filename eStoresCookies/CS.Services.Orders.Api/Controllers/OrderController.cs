5540bb0a259a9183edad69753589b081b1dff705
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-09 18:25:16 +0530

    Adding Logic

--- eStoresCookies/CS.Services.Orders.Api/Controllers/OrderController.cs
+++ eStoresCookies/CS.Services.Orders.Api/Controllers/OrderController.cs
@@ -2,6 +2,29 @@
 
 [Route("api/[controller]")]
 [ApiController]
-public class OrderController : ControllerBase
+public class OrderController(ILogger<OrderController> logger) : ControllerBase
 {
+
+    private readonly ILogger<OrderController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    [HttpPost("order")]
+    public async Task<ActionResult<Guid>> SubmitOrder(Order order, [FromServices] DaprClient daprClient)
+    {
+        _logger.LogInformation("Starting OrderController::SubmitOrder().");
+
+        order.Id = Guid.NewGuid();
+
+        foreach (OrderItem item in order.Items!)
+        {
+            var data = new { sku = item.ProductCode, quantity = item.Quantity };
+
+            var result = await daprClient.InvokeMethodAsync<object, dynamic>(HttpMethod.Post, "reservation-service", "reserve", data);
+
+            _logger.LogInformation($"sku: {result.GetProperty("sku")} === new quantity: {result.GetProperty("quantity")}");
+        }
+
+        _logger.LogInformation($"Completed OrderController::SubmitOrder(). Order Id: {order.Id}");
+
+        return order.Id;
+    }
 }
